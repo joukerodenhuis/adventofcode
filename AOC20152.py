4853f2ffb5420ab173fddb5f64cbfff631d5c9e6
joukerodenhuis <jsrodenhuis@hotmail.nl>
2024-05-25 12:11:42 +0200

    Day 2 and 3. Note that bote code files are changed for the second part of each puzzle

---
+++ AOC20152.py
@@ -0,0 +1,46 @@
+# This is for day 2 of the puzzles for the adventofcode2015 event
+
+l = 1
+w = 1
+h = 10
+area = 0
+Numbers = "1234567890"
+
+AOCinput = open("C:\\Users\\Jouke Rodenhuis\\Github\\adventofcode\\AOC20152.txt")
+
+def FileHandler(area):
+        a = 0
+        area = 0
+        dimensionlist = []
+        for line in AOCinput:
+            l = 0
+            w = 0
+            h = 0
+            for index, dimension in enumerate(line.strip().split("x")):
+                dimension == int(dimension)
+                if l == 0:
+                    l = int(dimension)
+                    continue
+                if w == 0:
+                    w = int(dimension)
+                    continue
+                if h == 0:
+                    h = int(dimension)
+                    continue
+            area = area + Calc(l,w,h)
+            a += 1
+        print(a)
+        return area
+            
+
+
+
+def Calc(l,w,h):
+    arealist = [ l, w, h ]
+    arealist.sort()
+    area = 2 * arealist[0] + 2 * arealist[1] + l * w * h
+    return area
+
+area = FileHandler(area)
+print(area)
+AOCinput.close
---
+++ AOC20153.py
@@ -0,0 +1,78 @@
+#The elf had too much eggnog apparently. I want whatever he's having.
+
+# Getting the directions and changing it to a normal string
+AOCinput = open("C:\\Users\\Jouke Rodenhuis\\Github\\adventofcode\\AOC20153.txt")
+AOCstr = ""
+for element in AOCinput:
+    AOCstr = AOCstr + element
+
+# The dictionary below is used for keeping track of the data. I am not happy with how its implemented in the PresentAmount function.
+# I don't yet know how to make that better
+TheDict = {"visitedhouses":[[0,0]],"amount":int(1),"xSanta":int(0),"xRobo":int(0),"ySanta":int(0),"yRobo":int(0),"SR":int(0)}
+
+# Main function. It loops through the directions of the AOCinput file and determines what needs to happen. 
+# Calls for another function below
+
+def CoordinateHandler(TheDict):
+    iteration = 1
+    for direction in AOCstr:
+        if TheDict["SR"] == 0:
+            TheDict["SR"] = 1
+            match direction:
+                case ">":
+                    TheDict["xSanta"] += 1
+                    TheDict = PresentAmount(TheDict)
+                    continue
+                case "<":
+                    TheDict["xSanta"] -= 1
+                    TheDict = PresentAmount(TheDict)
+                    continue
+                case "^":
+                    TheDict["ySanta"] += 1
+                    TheDict = PresentAmount(TheDict)
+                    continue
+                case "v":
+                    TheDict["ySanta"] -= 1
+                    TheDict = PresentAmount(TheDict)
+                    continue
+        if TheDict["SR"] == 1:
+            TheDict["SR"] = 0
+            match direction:
+                case ">":
+                    TheDict["xRobo"] += 1
+                    TheDict = PresentAmount(TheDict)
+                    continue
+                case "<":
+                    TheDict["xRobo"] -= 1
+                    TheDict = PresentAmount(TheDict)
+                    continue
+                case "^":
+                    TheDict["yRobo"] += 1
+                    TheDict = PresentAmount(TheDict)
+                    continue
+                case "v":
+                    TheDict["yRobo"] -= 1
+                    TheDict = PresentAmount(TheDict)
+                    continue
+                case _:
+                    continue
+        iteration += 1
+    return TheDict
+
+# The function below keeps track of how many houses received at least one present given a coordinate
+
+def PresentAmount(TheDict):
+    if TheDict["visitedhouses"].count([TheDict["xSanta"],TheDict["ySanta"]]) == 0 and TheDict["SR"] == 0:
+        TheDict["visitedhouses"].append([TheDict["xSanta"],TheDict["ySanta"]])
+        TheDict["amount"] += 1
+    if TheDict["visitedhouses"].count([TheDict["xRobo"],TheDict["yRobo"]]) == 0 and TheDict["SR"] == 1:
+        TheDict["visitedhouses"].append([TheDict["xRobo"],TheDict["yRobo"]])
+        TheDict["amount"] += 1
+    return TheDict
+
+TheDict = CoordinateHandler(TheDict)
+print("")
+print(TheDict["visitedhouses"])
+print(TheDict["amount"])
+print("")
+
